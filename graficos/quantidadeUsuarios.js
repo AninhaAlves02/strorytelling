1352ed1b6d296782ab705ce14e7e8f4f67f75680
AninhaAlves02 <125603285+AninhaAlves02@users.noreply.github.com>
2024-10-02 14:05:40 -0300

    0210

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,38 @@
+import { getCSS, tickConfig } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = https;//github.com/AninhaAlves02/strorytelling
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const laytout = [
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociis com mais usuarios',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+        }
+    ]
+    
+}
